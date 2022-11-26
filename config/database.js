e6295132d0e8b4d84dd1b442b26d99a7ef6faab5
sanditya12 <sanditya2002@gmail.com>
2022-11-26 22:18:05 +0100

    fix

--- config/database.js
+++ config/database.js
@@ -1,13 +1,6 @@
 module.exports = ({ env }) => ({
   connection: {
-    client: 'postgres',
-    connection: {
-      host: env('DATABASE_HOST', 'tiny.db.elephantsql.com'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'db'),
-      user: env('DATABASE_USERNAME', 'inpmqkjv'),
-      password: env('DATABASE_PASSWORD', 'kR2TG9aBZyRu5zj48LgySTDgKj66zA2L'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
+    client: "postgres",
+    connection: {},
   },
 });
