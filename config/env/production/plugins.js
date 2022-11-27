b78a000c73c6ef2117177a648818bd22bdf94a88
sanditya12 <sanditya2002@gmail.com>
2022-11-27 08:22:26 +0100

    fix storage space provider

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,18 +2,13 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: "strapi-provider-upload-do-virtuali",
+      provider: "strapi-provider-upload-do",
       providerOptions: {
         key: env("DO_SPACE_ACCESS_KEY"),
         secret: env("DO_SPACE_SECRET_KEY"),
         endpoint: env("DO_SPACE_ENDPOINT"),
         space: env("DO_SPACE_BUCKET"),
         acl: "public-read", // default ACL
-      },
-      actionOptions: {
-        upload: {},
-        uploadStream: {},
-        delete: {},
       },
     },
   },
