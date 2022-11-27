5205b6bdf5d479d95946fc3fdeae2b1a17a3223b
sanditya12 <sanditya2002@gmail.com>
2022-11-27 08:17:40 +0100

    fix storage

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,13 +2,19 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: "strapi-provider-upload-do",
+      provider: "strapi-provider-upload-do-virtuali",
       providerOptions: {
-        key: process.env.DO_SPACE_ACCESS_KEY,
-        secret: process.env.DO_SPACE_SECRET_KEY,
-        endpoint: process.env.DO_SPACE_ENDPOINT,
-        space: process.env.DO_SPACE_BUCKET,
-        directory: "media", // optional
+        key: env("DO_SPACE_ACCESS_KEY"),
+        secret: env("DO_SPACE_SECRET_KEY"),
+        endpoint: env("DO_SPACE_ENDPOINT"),
+        space: env("DO_SPACE_BUCKET"),
+        cdn: env("DO_SPACE_CDN"),
+        acl: "public-read", // default ACL
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
       },
     },
   },
