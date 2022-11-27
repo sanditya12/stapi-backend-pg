3a76135b673c9c5f2b3d9e4a324ffc76b8e2cb0a
sanditya12 <sanditya2002@gmail.com>
2022-11-27 08:18:23 +0100

    fix storage cdn env

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -8,7 +8,6 @@ module.exports = ({ env }) => ({
         secret: env("DO_SPACE_SECRET_KEY"),
         endpoint: env("DO_SPACE_ENDPOINT"),
         space: env("DO_SPACE_BUCKET"),
-        cdn: env("DO_SPACE_CDN"),
         acl: "public-read", // default ACL
       },
       actionOptions: {
