09bf30844b8e6a7feeb86f038075995c671c70b0
sanditya12 <sanditya2002@gmail.com>
2022-11-27 08:52:29 +0100

    fix spaces for strapi v4

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -2,13 +2,12 @@ module.exports = ({ env }) => ({
   // ...
   upload: {
     config: {
-      provider: "strapi-provider-upload-do",
+      provider: "strapi-provider-upload-dos",
       providerOptions: {
         key: env("DO_SPACE_ACCESS_KEY"),
         secret: env("DO_SPACE_SECRET_KEY"),
         endpoint: env("DO_SPACE_ENDPOINT"),
         space: env("DO_SPACE_BUCKET"),
-        acl: "public-read", // default ACL
       },
     },
   },
