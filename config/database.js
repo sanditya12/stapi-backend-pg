e4b0b54a1567acfde7d09116466f5877a9f0e2e9
sanditya12 <sanditya2002@gmail.com>
2022-11-27 07:26:49 +0100

    created content types

--- config/database.js
+++ config/database.js
@@ -1,6 +1,15 @@
+const path = require("path");
+
 module.exports = ({ env }) => ({
   connection: {
-    client: "postgres",
-    connection: {},
+    client: "sqlite",
+    connection: {
+      filename: path.join(
+        __dirname,
+        "..",
+        env("DATABASE_FILENAME", ".tmp/data.db")
+      ),
+    },
+    useNullAsDefault: true,
   },
 });
